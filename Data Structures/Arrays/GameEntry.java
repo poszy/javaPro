4efdebde7cf2676e78137d38a4b706ca928b0630
lu <pena.m.luis@gmail.com>
2017-06-27 16:24:40 -0700

    added array and game files

---
+++ Data Structures/Arrays/GameEntry.java
@@ -0,0 +1,24 @@
+public class GameEntry{
+
+  private String name; // name f the person earning this score
+
+  private int score;
+
+  // Constructs a game enttry with given parameters
+
+  public GameEntry(String n, int s){
+    name  = n;
+    score = s;
+
+  } // end Game Entry
+
+  public String getName(){return name;}
+  public int getScore(){return score;}
+
+  public String toString() {
+
+    return"(" + name +", " + score + ")";
+
+  }
+
+}
